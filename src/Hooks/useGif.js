3cb886a83da7332b360240019891d26b72fa8c35
Stephen Guo <wetech16@gmail.com>
2020-09-25 17:19:30 -0700

    -removed hooks

--- src/Hooks/useGif.js
+++ src/Hooks/useGif.js
@@ -6,19 +6,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 const useGif = () => {
   const [gif, setGif] = useState("");
 
-  const fetchGif = async () => {
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-
-    const { data } = await axios.get(url);
-
-    const imageSrc = data.data.images.downsized_large.url;
-
-    setGif(imageSrc);
-  };
-
-  useEffect(() => {
-    fetchGif();
-  }, []);
+  const fetchGif = async () => {};
 
   return { gif, fetchGif };
 };
--- src/Hooks/useTag.js
+++ src/Hooks/useTag.js
@@ -6,15 +6,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 function useTag(tag) {
   const [tagGif, settagGif] = useState("");
 
-  const fetchTagGif = async (tag) => {
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
-    const { data } = await axios.get(url);
-    const imageSrc = data.data.images.downsized_large.url;
-    settagGif(imageSrc);
-  };
-  useEffect(() => {
-    fetchTagGif(tag);
-  }, [tag]);
+  const fetchTagGif = async (tag) => {};
 
   return { tagGif, fetchTagGif };
 }
